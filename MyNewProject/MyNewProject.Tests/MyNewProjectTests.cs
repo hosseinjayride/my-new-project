c0e6d95f0d0721da5e30276df74b9a7db8d00c9c
Hossein Abbasi <hossein@jayride.com>
2016-12-21 16:16:14 +1100

    added test project

---
+++ MyNewProject/MyNewProject.Tests/MyNewProjectTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace MyNewProject.Tests
+{
+    [TestFixture]
+    public class MyNewProjectTests
+    {
+        [Test]
+        public void Values_Should_Be_The_Same()
+        {
+            Assert.AreEqual("The value should be this", "This is the actual value");
+        }
+    }
+}
