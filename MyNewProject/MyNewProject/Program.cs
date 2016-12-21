b759d44a0606178591d8121734a607d755e39d36
Hossein Abbasi <hossein@jayride.com>
2016-12-21 16:10:54 +1100

    changed main program logic

--- MyNewProject/MyNewProject/Program.cs
+++ MyNewProject/MyNewProject/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MyNewProject
 {
@@ -10,6 +6,7 @@ namespace MyNewProject
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hi");
         }
     }
 }
